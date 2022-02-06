2d46f92ffb688c255480a7ebdc6fc1234526f0d5
Justin Luciano <justinlluciano@gmail.com>
2022-02-05 16:28:26 -0800

    commit tree 2e507e9fa4d29f61c05485343c385b4641390ab1

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,4 +1,4 @@
-# Something is missing here
+require_relative './plant'
 
 class Garden
   attr_accessor :name
@@ -8,9 +8,7 @@ class Garden
   end
 
   def plants
-    Plant.all.select do |plant|
-      plant.garden == self
-    end
+    Plant.all.select { |plant| plant.garden == self }
   end
 end
 
